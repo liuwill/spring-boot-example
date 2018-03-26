bb818e647c80b672838b7e3a2dabb8e8cdf3f0f8
liuwill <liuwei_will@qq.com>
2018-03-26 14:24:37 +0800

    init spring boot config demo

---
+++ spring-boot-config/src/main/java/com/liuwill/example/config/ConfigApplication.java
@@ -0,0 +1,30 @@
+package com.liuwill.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Liu Will - liuwill@live.com on 2018/3/24.
+ * Copyright (c) 2012-2017 All Rights Reserved.
+ *
+ * @author: liuwill@live.com liuwill
+ * @date 2018/3/24
+ * @desc
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class);
+    }
+}
