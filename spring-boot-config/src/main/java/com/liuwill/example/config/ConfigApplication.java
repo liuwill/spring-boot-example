fa7245e4ba93bda31b63ff3547f4f606e8bd3e05
liuwill <liuwei_will@qq.com>
2018-03-26 14:51:07 +0800

    change banner for spring boot config demo

--- spring-boot-config/src/main/java/com/liuwill/example/config/ConfigApplication.java
+++ spring-boot-config/src/main/java/com/liuwill/example/config/ConfigApplication.java
@@ -13,6 +13,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ *   _____      _       ____      ____  _   __   __
+ * |_   _|    (_)     |_  _|    |_  _|(_) [  | [  |
+ * | |      __  __   _\ \  /\  / /  __   | |  | |
+ * | |   _ [  |[  | | |\ \/  \/ /  [  |  | |  | |
+ * _| |__/ | | | | \_/ |,\  /\  /    | |  | |  | |
+ * |________|[___]'.__.'_/ \/  \/    [___][___][___]
+ *
  * Created by Liu Will - liuwill@live.com on 2018/3/24.
  * Copyright (c) 2012-2017 All Rights Reserved.
  *
@@ -22,7 +29,6 @@ import java.util.Map;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-@RestController
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class);
