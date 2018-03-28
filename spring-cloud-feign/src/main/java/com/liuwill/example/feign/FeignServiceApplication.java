297c92d85470cc93d1809e1d3c2961e2d7ad01f7
liuwill <liuwei_will@qq.com>
2018-03-28 20:21:25 +0800

    add feign cloud

---
+++ spring-cloud-feign/src/main/java/com/liuwill/example/feign/FeignServiceApplication.java
@@ -0,0 +1,21 @@
+package com.liuwill.example.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by Liu Will - liuwill@live.com on 2018/3/27.
+ * Copyright (c) 2012-2017 All Rights Reserved.
+ *
+ * @author: liuwill@live.com liuwill
+ * @date 2018/3/27
+ * @desc
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FeignServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServiceApplication.class, args);
+    }
+}
